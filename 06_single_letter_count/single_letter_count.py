373c889d943ee948422d867c0a396c7049fc3a22
Nabin <nkbhaie2017@gmail.com>
2021-03-11 15:05:43 -0700

    add singel letter count function

--- 06_single_letter_count/single_letter_count.py
+++ 06_single_letter_count/single_letter_count.py
@@ -10,3 +10,7 @@ def single_letter_count(word, letter):
         >>> single_letter_count("Hello World", 'l')
         3
     """
+    return word.lower().count(letter)
+print(single_letter_count('Hello World', 'h'))
+print(single_letter_count('Hello World', 'z'))
+print(single_letter_count("Hello World", 'l'))
