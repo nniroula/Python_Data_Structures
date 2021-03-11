d55e8c2bda8600d793a206853efb9109d2c74642
Nabin <nkbhaie2017@gmail.com>
2021-03-11 13:24:29 -0700

    add week_day

--- 02_weekday_name/weekday_name.py
+++ 02_weekday_name/weekday_name.py
@@ -12,3 +12,14 @@ def weekday_name(day_of_week):
         >>> weekday_name(9)
         >>> weekday_name(0)
     """
+
+    days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    if day_of_week < 1 or day_of_week > 7:
+        return None
+    else:
+        return days[day_of_week - 1]
+
+print(weekday_name(1))
+print(weekday_name(7))
+print(weekday_name(9))
+print(weekday_name(0))
