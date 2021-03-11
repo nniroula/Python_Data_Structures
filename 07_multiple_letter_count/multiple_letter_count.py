4daff2a7217f3423f73a9d60ccefb7f115ffe331
Nabin <nkbhaie2017@gmail.com>
2021-03-11 15:41:14 -0700

    add multiple letter count method

--- 07_multiple_letter_count/multiple_letter_count.py
+++ 07_multiple_letter_count/multiple_letter_count.py
@@ -7,3 +7,10 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+    dict1 = {}
+    for char in phrase:
+        # dict1[char] = dict1.get(char, 0) + 1
+        dict1[char] = dict1.get(char, 0) + 1
+    return dict1
+print(multiple_letter_count('yay'))
+print(multiple_letter_count('Yay'))
