b7cb2c8a2ef49d80aee85cd334630c6209bfa8d1
Nabin <nkbhaie2017@gmail.com>
2021-03-12 13:25:34 -0700

    add two list dictionary function

--- 32_two_list_dictionary/two_list_dictionary.py
+++ 32_two_list_dictionary/two_list_dictionary.py
@@ -15,3 +15,28 @@ def two_list_dictionary(keys, values):
         >>> two_list_dictionary(['a', 'b', 'c'], [1, 2, 3, 4])
         {'a': 1, 'b': 2, 'c': 3}
    """
+    dict1 = {}
+
+    # if len(keys) < len(values):
+    #     new_values = values.copy()
+    #     for i in range(len(keys)):
+    #         if i > len(values):
+    #             new_values.append(None)       
+    #             for i in range(len(keys)):
+    #                 dict1[keys[i]] = values[i]
+    #     print(new_values)
+
+    for i in range(len(keys)):
+        dict1[keys[i]] = values[i]
+    return dict1
+
+    """
+     out = {}
+    for idx, val in enumerate(keys):
+        out[val] = values[idx] if idx < len(values) else None
+    return out
+    """
+
+print(two_list_dictionary(['x', 'y', 'z'], [9, 8, 7]))
+#print(two_list_dictionary(['a', 'b', 'c', 'd'], [1, 2, 3]))
+print(two_list_dictionary(['a', 'b', 'c'], [1, 2, 3, 4]))
