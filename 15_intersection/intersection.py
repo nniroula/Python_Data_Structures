4bd14d10e4a3df8fd47c7389ce873a1159aba5fd
Nabin <nkbhaie2017@gmail.com>
2021-03-11 19:15:01 -0700

    add list intersection method

--- 15_intersection/intersection.py
+++ 15_intersection/intersection.py
@@ -13,3 +13,16 @@ def intersection(l1, l2):
         >>> intersection([1, 2, 3], [4, 5, 6])
         []
     """
+    # turn into set, find intersection and turn back to set
+    set1 = set(l1)
+    set2 = set(l2)
+    set3 = set1 & set2
+    # pop items from set and append them in list
+    lst = []
+    for item in set3:
+        lst.append(item)
+    return lst
+print(intersection([1, 2, 3], [2, 3, 4]))
+print(intersection([1, 2, 3], [3, 4]))
+print(intersection([1, 2, 3], [4, 5, 6]))
+    
