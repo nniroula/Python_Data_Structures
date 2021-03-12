bd7e41bba9c6a38242cf6e684b8f0cf6e25989cd
Nabin <nkbhaie2017@gmail.com>
2021-03-12 14:00:08 -0700

    add same frequency method

--- 34_same_frequency/same_frequency.py
+++ 34_same_frequency/same_frequency.py
@@ -10,3 +10,25 @@ def same_frequency(num1, num2):
         >>> same_frequency(1212, 2211)
         True
     """
+    
+    num1 = str(num1)
+    num2 = str(num2)
+
+    count1 = 0
+    count2 = 0
+    for number in num1:
+        if number:
+            count1 = count1 + 1
+    for numbs in num2:
+        if numbs:
+            count2 = count2 + 1
+    # print(count1)
+    # print(count2)
+    if count1 == count2:
+        return True
+    else:
+        return False
+
+print(same_frequency(551122, 221515))
+print(same_frequency(321142, 3212215))
+print(same_frequency(1212, 2211))
