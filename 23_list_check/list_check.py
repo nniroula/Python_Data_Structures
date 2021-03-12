aa7288291466c65d7876c88eeb5ef9ca3a545c59
Nabin <nkbhaie2017@gmail.com>
2021-03-12 09:22:44 -0700

    add isinstance list method

--- 23_list_check/list_check.py
+++ 23_list_check/list_check.py
@@ -1,9 +1,19 @@
 def list_check(lst):
     """Are all items in lst a list?
 
-        >>> list_check([[1], [2, 3]])
+        >>> sum_floats([1, 2, 3])
         True
 
         >>> list_check([[1], "nope"])
         False
     """
+    type_holder = True
+    for items in lst:
+        # print(type(items))
+        if isinstance(items, list):
+            type_holder = False
+    return type_holder
+
+sum_floats = [1, 2, 3]
+print(list_check(sum_floats))
+print(list_check([[1], "nope"]))
