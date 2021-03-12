a24438dad640760c502f7a9c5dbb263e1ea83c5d
Nabin <nkbhaie2017@gmail.com>
2021-03-12 10:16:20 -0700

    vowel count

--- 26_vowel_count/vowel_count.py
+++ 26_vowel_count/vowel_count.py
@@ -7,3 +7,20 @@ def vowel_count(phrase):
         >>> vowel_count('HOW ARE YOU? i am great!') 
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+    new_dict = dict()
+    for letters in phrase:
+        letters = letters.lower()
+        if letters == 'a': 
+            new_dict[letters] = phrase.count(letters)
+        elif letters == 'e':
+             new_dict[letters] = phrase.count(letters)
+        elif letters == 'i':
+             new_dict[letters] = phrase.count(letters)
+        elif letters == 'o':
+             new_dict[letters] = phrase.count(letters)
+        elif letters == 'u':
+             new_dict[letters] = phrase.count(letters)
+
+    return new_dict
+print(vowel_count('rithm school'))
+print(vowel_count('HOW ARE YOU? i am great!'))
