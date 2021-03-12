0fd450b344278d373ccdea3e79e6a94056635e66
Nabin <nkbhaie2017@gmail.com>
2021-03-12 06:19:26 -0700

    add hobbies intersection

--- 19_friend_date/friend_date.py
+++ 19_friend_date/friend_date.py
@@ -16,3 +16,19 @@ def friend_date(a, b):
         >>> friend_date(sauron, gandalf)
         True
     """
+    # conver hobbies into list and then convert list into set to find any intersection
+    a_hobby = a[-1]
+    b_hobby = b[-1]
+    set1 = set(a_hobby)
+    set2 = set(b_hobby)
+    if set1 & set2:
+        return True
+    else:
+        return False
+
+elmo = ('Elmo', 5, ['hugging', 'being nice'])
+sauron = ('Sauron', 5000, ['killing hobbits', 'chess'])
+gandalf = ('Gandalf', 10000, ['waving wands', 'chess'])
+
+print(friend_date(elmo, sauron))
+print(friend_date(sauron, gandalf))
