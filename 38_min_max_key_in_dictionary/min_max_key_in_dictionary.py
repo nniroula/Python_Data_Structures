1836183b94a557da68f2d980da9942a554ff4f86
Nabin <nkbhaie2017@gmail.com>
2021-03-12 14:44:10 -0700

    add min and max keys

--- 38_min_max_key_in_dictionary/min_max_key_in_dictionary.py
+++ 38_min_max_key_in_dictionary/min_max_key_in_dictionary.py
@@ -9,3 +9,9 @@ def min_max_keys(d):
         >>> min_max_keys({"apple": "red", "cherry": "red", "berry": "blue"})
         ('apple', 'cherry')
     """
+
+    keys_in_d = d.keys()
+    return (min(keys_in_d ), max(keys_in_d ))
+
+print(min_max_keys({2: 'a', 7: 'b', 1: 'c', 10: 'd', 4: 'e'}))
+print(min_max_keys({"apple": "red", "cherry": "red", "berry": "blue"}))
