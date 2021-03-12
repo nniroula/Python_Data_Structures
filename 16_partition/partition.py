ad4d9c86fa51a9c8e56c55f698d0fc90a1941510
Nabin <nkbhaie2017@gmail.com>
2021-03-11 21:59:17 -0700

    add partition method

--- 16_partition/partition.py
+++ 16_partition/partition.py
@@ -19,3 +19,21 @@ def partition(lst, fn):
         >>> partition(["hi", None, 6, "bye"], is_string)
         [['hi', 'bye'], [None, 6]]
     """
+    member = []
+    non_member = []
+    for items in lst:
+        if fn(items):
+            member.append(items)
+        else:
+            non_member.append(items)
+    return [member, non_member]
+
+def is_even(num):
+    return num % 2 == 0
+print(partition([1, 2, 3, 4], is_even))
+
+def is_string(el):
+    return isinstance(el, str)
+print(partition(["hi", None, 6, "bye"], is_string))
+    
+        
