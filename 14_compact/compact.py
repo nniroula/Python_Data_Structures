a1becb1fc14c84a208117310fa381c1660476228
Nabin <nkbhaie2017@gmail.com>
2021-03-11 18:54:52 -0700

    add remove none true values

--- 14_compact/compact.py
+++ 14_compact/compact.py
@@ -4,3 +4,11 @@ def compact(lst):
         >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
         [1, 2, 'All done']
     """
+    lst1 = [0, False, '', None, []]
+    new_lst = []
+    for item in lst:
+        if item:
+            new_lst.append(item)
+    return new_lst
+print(compact([0, 1, 2, '', [], False, (), None, 'All done']))
+
