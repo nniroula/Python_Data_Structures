23d9fde78c8a3b8ff2f45132be13f9fdb3ff669f
Nabin <nkbhaie2017@gmail.com>
2021-03-12 08:56:50 -0700

    add isinstacne method to check if a number is float

--- 22_sum_floats/sum_floats.py
+++ 22_sum_floats/sum_floats.py
@@ -11,3 +11,11 @@ def sum_floats(nums):
     # hint: to find out if something is a float, you should use the
     # "isinstance" function --- research how to use this to find out
     # if something is a float!
+    sum = 0
+    for items in nums:
+        if isinstance(items, float):
+            sum = sum + items
+    return sum
+
+print(sum_floats([1.5, 2.4, 'awesome', [], 1]))
+print(sum_floats([1, 2, 3]))
