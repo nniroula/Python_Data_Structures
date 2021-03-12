08890a71b02f3d50a698e6dc568266c86adf408a
Nabin <nkbhaie2017@gmail.com>
2021-03-11 22:45:21 -0700

    add calculate method

--- 18_calculate/calculate.py
+++ 18_calculate/calculate.py
@@ -8,21 +8,30 @@ def calculate(operation, a, b, make_int=False, message='The result is'):
 
     Performs math operation (truncating if make_int), then returns as
     "[message] [result]"
-
         >>> calculate('add', 2.5, 4)
         'The result is 6.5'
-
         >>> calculate('subtract', 4, 1.5, make_int=True)
         'The result is 2'
-
         >>> calculate('multiply', 1.5, 2)
         'The result is 3.0'
-
         >>> calculate('divide', 10, 4, message='I got')
         'I got 2.5'
-
     If a valid operation isn't provided, return None.
-
         >>> calculate('foo', 2, 3)
         
     """
+    if operation == "add":
+        return f"{message} {a + b}"
+    elif operation == "subtract" and make_int == True:
+        return f"{message} {int(a - b)}"
+    elif operation == "multiply":
+        return f"{message} {a*b}"
+    elif operation == "divide":
+        return f"{message} {a/b}"
+    else:
+        return None
+print(calculate('add', 2.5, 4))
+print(calculate('subtract', 4, 1.5, make_int=True))
+print(calculate('multiply', 1.5, 2))
+print(calculate('divide', 10, 4, message='I got'))
+print(calculate('foo', 2, 3))
