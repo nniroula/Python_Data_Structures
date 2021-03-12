7677f5b24204d70f09d0c02fcb551a885e091c20
Nabin <nkbhaie2017@gmail.com>
2021-03-12 14:31:25 -0700

    add find duplicate methods

--- 36_find_the_duplicate/find_the_duplicate.py
+++ 36_find_the_duplicate/find_the_duplicate.py
@@ -13,3 +13,10 @@ def find_the_duplicate(nums):
         >>> find_the_duplicate([2, 1, 3, 4]) is None
         True
     """
+    for numbers in nums:
+        if nums.count(numbers)> 1:
+            return numbers
+                   
+print(find_the_duplicate([1, 2, 1, 4, 3, 12]))
+print(find_the_duplicate([6, 1, 9, 5, 3, 4, 9]))
+print(find_the_duplicate([2, 1, 3, 4]) is None)
