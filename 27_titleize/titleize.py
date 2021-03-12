6e4704c6dd13bfa8ebbbcc983cb1cc6d914048a2
Nabin <nkbhaie2017@gmail.com>
2021-03-12 10:31:53 -0700

    add titleize function

--- 27_titleize/titleize.py
+++ 27_titleize/titleize.py
@@ -7,3 +7,7 @@ def titleize(phrase):
         >>> titleize('oNLy cAPITALIZe fIRSt')
         'Only Capitalize First'
     """
+    str1 = phrase.title() # This is a built in python function .title()
+    return str1
+print(titleize('this is awesome'))
+print(titleize('oNLy cAPITALIZe fIRSt'))
