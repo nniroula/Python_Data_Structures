98aba62387ea8ab6e519601d35d44ea6ce337e01
Nabin <nkbhaie2017@gmail.com>
2021-03-12 07:13:43 -0700

    add full name method

--- 21_extract_full_name/extract_full_name.py
+++ 21_extract_full_name/extract_full_name.py
@@ -14,3 +14,13 @@ def extract_full_names(people):
         >>> extract_full_names(names)
         ['Ada Lovelace', 'Grace Hopper']
     """
+    new_list = []
+    for person in people:
+        new_list.append(person.get("first") + " " + person.get("last")) 
+    return new_list
+
+names = [
+    {'first': 'Ada', 'last': 'Lovelace'},
+    {'first': 'Grace', 'last': 'Hopper'},
+]
+print(extract_full_names(names))
